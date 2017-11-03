16a7ecfec394d61beb85bd3ae5cf134e22c9ac1d
Andrew Nichols <andrew@frenata.net>
2017-11-03 12:39:21 -0400

    Add test data; abstract error handling.

--- parser.go
+++ parser.go
@@ -1,10 +1,38 @@
 package metro2
 
+import (
+	"errors"
+	"fmt"
+	"strconv"
+)
+
 type Header struct {
 }
 
-func parseFixedHeader(str string) (*Header, error) {
+func parseFixedHeader(source string) (*Header, error) {
 	header := &Header{}
+	e := errParser{}
 
-	return header, nil
+	rdw := e.parseNumeric(source[0:4])
+	if rdw != len(source) {
+		return nil, errors.New(fmt.Sprintf("Reported record length: (%d) does not match actual length of record: (%d).", rdw, len(source)))
+	}
+
+	return header, e.err
+}
+
+type errParser struct {
+	err error
+}
+
+func (e *errParser) parseNumeric(str string) (n int) {
+	if e.err != nil {
+		return n
+	}
+
+	n, err := strconv.Atoi(str)
+	if err != nil {
+		e.err = err
+	}
+	return n
 }
--- parser_test.go
+++ parser_test.go
@@ -1,13 +1,43 @@
 package metro2
 
-import "testing"
+import (
+	"io/ioutil"
+	"testing"
+)
 
-func TestParseFixedHeader(t *testing.T) {
-	file := "426invalidheader"
+func TestInvalidLength(t *testing.T) {
+	file := "0426invalidheader"
 
 	_, err := parseFixedHeader(file)
 
 	if err == nil {
-		t.Fatalf("Failed parsing a header with incorrect RDW: %s", err)
+		t.Fatal("File with invalid length should have failed to parse: instead, no error was returned")
 	}
+}
+
+func TestBadFormatLength(t *testing.T) {
+	file := "15invalidheader"
+
+	_, err := parseFixedHeader(file)
+
+	if err == nil {
+		t.Fatalf("File with invalid numeric field did not return an error: %s", err)
+	}
+}
+
+func TestValidLength(t *testing.T) {
+	file := "0015validheader"
+
+	_, err := parseFixedHeader(file)
+
+	if err != nil {
+		t.Fatalf("File with valid length failed to parse: %s", err)
+	}
+}
+
+func TestValidFile(t *testing.T) {
+	file, _ := ioutil.ReadFile("header.txt")
+
+	t.Log(len(file))
+
 }
